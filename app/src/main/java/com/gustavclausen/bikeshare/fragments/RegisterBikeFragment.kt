d58c91b8531fc8364e4a136c210950fde97086cf
Gustav Kofoed Clausen <gucl@itu.dk>
2019-04-04 15:32:09 +0200

    Layout for register bike activity

---
+++ app/src/main/java/com/gustavclausen/bikeshare/fragments/RegisterBikeFragment.kt
@@ -0,0 +1,14 @@
+package com.gustavclausen.bikeshare.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gustavclausen.bikeshare.R
+
+class RegisterBikeFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_register_bike, container, false)
+    }
+}
