34b9230c4a4bb70c024464c90fe20b5122bd18c7
Gustav Kofoed Clausen <gucl@itu.dk>
2019-04-07 22:26:53 +0200

    User Realm-setup; Startup creation

--- app/src/main/java/com/gustavclausen/bikeshare/BikeShareApplication.kt
+++ app/src/main/java/com/gustavclausen/bikeshare/BikeShareApplication.kt
@@ -1,13 +1,22 @@
 package com.gustavclausen.bikeshare
 
 import android.app.Application
+import android.content.Context
+import com.gustavclausen.bikeshare.models.UserDB
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
 class BikeShareApplication : Application() {
+
+    companion object {
+        const val PREF_USER_FILE = "user"
+        const val PREF_USER_ID = "user_id"
+    }
+
     override fun onCreate() {
         super.onCreate()
 
+        // Realm configuration
         Realm.init(this)
         Realm.setDefaultConfiguration(
             RealmConfiguration.Builder()
@@ -15,5 +24,18 @@ class BikeShareApplication : Application() {
                 .deleteRealmIfMigrationNeeded()
                 .build()
         )
+
+        // User account configuration
+        val userPreferences = applicationContext.getSharedPreferences(PREF_USER_FILE, Context.MODE_PRIVATE)
+        val registeredUserId = userPreferences.getString(PREF_USER_ID, null)
+
+        // Create dummy user account if it is the first time user starts the app
+        if (registeredUserId == null) {
+            val newUserId = UserDB.get().addUser("Frank Castle")
+
+            val editor = userPreferences.edit()
+            editor.putString(PREF_USER_ID, newUserId)
+            editor.apply()
+        }
     }
 }
---
+++ app/src/main/java/com/gustavclausen/bikeshare/models/UserDB.kt
@@ -0,0 +1,28 @@
+package com.gustavclausen.bikeshare.models
+
+import io.realm.Realm
+import java.util.*
+
+class UserDB private constructor() {
+    private val mRealm = Realm.getDefaultInstance()!!
+
+    companion object {
+        private val db: UserDB = UserDB()
+
+        fun get(): UserDB = db
+    }
+
+    fun addUser(fullName: String): String? {
+        val newUserId: String? = UUID.randomUUID().toString()
+
+        mRealm.executeTransaction { realm ->
+            val user = realm.createObject(User::class.java, newUserId)
+            user.fullName = fullName
+        }
+
+        return newUserId
+    }
+
+    fun getUser(userId: String): User? =
+        mRealm.where(User::class.java).equalTo("id", userId).findFirst()
+}
