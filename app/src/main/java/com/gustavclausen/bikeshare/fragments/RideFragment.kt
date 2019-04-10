cae9e27075fb010ec2f0c0ec1abcf15a7e7c2b02
Gustav Kofoed Clausen <gucl@itu.dk>
2019-04-10 18:48:45 +0200

    Add working map fragment

--- app/src/main/java/com/gustavclausen/bikeshare/fragments/RideFragment.kt
+++ app/src/main/java/com/gustavclausen/bikeshare/fragments/RideFragment.kt
@@ -5,9 +5,16 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.gustavclausen.bikeshare.helpers.MapStateManager
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.gustavclausen.bikeshare.R
 
-class RideFragment : Fragment() {
+class RideFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +23,28 @@ class RideFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val mapFragment = SupportMapFragment.newInstance()
+        mapFragment.getMapAsync(this)
+
+        childFragmentManager.beginTransaction()
+                            .replace(R.id.map, mapFragment)
+                            .commit()
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ride, container, false)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        MapStateManager.saveMapState(mMap, context!!)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.setMaxZoomPreference(25.0f)
+
+        val lastPosition = MapStateManager.getSavedMapState(context!!)
+        val update = CameraUpdateFactory.newCameraPosition(lastPosition)
+        mMap.moveCamera(update)
     }
 }
---
+++ app/src/main/java/com/gustavclausen/bikeshare/helpers/MapStateManager.kt
@@ -0,0 +1,46 @@
+package com.gustavclausen.bikeshare.helpers
+
+import android.content.Context
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+
+// Source: https://stackoverflow.com/questions/34636722/android-saving-map-state-in-google-map (accessed 2019-04-10)
+class MapStateManager {
+    companion object {
+        private const val LONGITUDE = "longitude"
+        private const val LATITUDE  = "latitude"
+        private const val ZOOM      = "zoom"
+        private const val BEARING   = "bearing"
+        private const val TILT      = "tilt"
+
+        private const val PREFS_NAME = "mapCameraState"
+
+        fun saveMapState(map: GoogleMap, context: Context) {
+            val mapStatePrefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+            val editor = mapStatePrefs.edit()
+
+            val cameraPosition = map.cameraPosition
+            editor.putFloat(LATITUDE, cameraPosition.target.latitude.toFloat())
+            editor.putFloat(LONGITUDE, cameraPosition.target.longitude.toFloat())
+            editor.putFloat(ZOOM, cameraPosition.zoom)
+            editor.putFloat(TILT, cameraPosition.tilt)
+            editor.putFloat(BEARING, cameraPosition.bearing)
+            editor.apply()
+        }
+
+        fun getSavedMapState(context: Context): CameraPosition {
+            val mapStatePrefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+
+            val latitude = mapStatePrefs.getFloat(LATITUDE, 0f)
+            val longitude = mapStatePrefs.getFloat(LONGITUDE, 0f)
+            val target = LatLng(latitude.toDouble(), longitude.toDouble())
+
+            val zoom = mapStatePrefs.getFloat(ZOOM, 0f)
+            val bearing = mapStatePrefs.getFloat(BEARING, 0f)
+            val tilt = mapStatePrefs.getFloat(TILT, 0f)
+
+            return CameraPosition(target, zoom, tilt, bearing)
+        }
+    }
+}
