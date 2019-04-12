d6d50c3bc0b80890f87f8b1ca72f6edb43be55e5
Gustav Kofoed Clausen <gucl@itu.dk>
2019-04-12 16:12:43 +0200

    Add overview layout skeleton

---
+++ app/src/main/java/com/gustavclausen/bikeshare/fragments/BikesOverviewFragment.kt
@@ -0,0 +1,15 @@
+package com.gustavclausen.bikeshare.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gustavclausen.bikeshare.R
+
+class BikesOverviewFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_bikes_overview, container, false)
+    }
+}
--- app/src/main/java/com/gustavclausen/bikeshare/fragments/OverviewFragment.kt
+++ app/src/main/java/com/gustavclausen/bikeshare/fragments/OverviewFragment.kt
@@ -2,11 +2,15 @@ package com.gustavclausen.bikeshare.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.gustavclausen.bikeshare.R
+import kotlinx.android.synthetic.main.fragment_overview.*
+import java.util.ArrayList
 
 class OverviewFragment : Fragment() {
 
@@ -17,7 +21,34 @@ class OverviewFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_overview, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val viewPagerAdapter = ViewPagerAdapter(childFragmentManager)
+        viewPagerAdapter.addFragment(BikesOverviewFragment(), getString(R.string.title_bikes_overview))
+        viewPagerAdapter.addFragment(RidesOverviewFragment(), getString(R.string.title_rides_overview))
+        overview_view_pager_adapter.adapter = viewPagerAdapter
+
+        overview_tabs.setupWithViewPager(overview_view_pager_adapter)
+    }
+
+    class ViewPagerAdapter(fragmentManager: FragmentManager): FragmentPagerAdapter(fragmentManager) {
+
+        private val mFragmentList = ArrayList<Fragment>()
+        private val mFragmentTitleList = ArrayList<String>()
+
+        override fun getItem(position: Int) = mFragmentList[position]
+
+        override fun getCount() = mFragmentList.size
+
+        fun addFragment(fragment: Fragment, title: String) {
+            mFragmentList.add(fragment)
+            mFragmentTitleList.add(title)
+        }
+
+        override fun getPageTitle(position: Int) = mFragmentTitleList[position]
     }
 }
